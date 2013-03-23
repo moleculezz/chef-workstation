f03b9e822e68f8e709d2ea64c19b2d27e6f26454
G. Arends <gdarends@gmail.com>
2013-03-23 12:31:17 +0100

    add rbenv recipe

---
+++ cookbooks/workstation/recipes/rbenv.rb
@@ -0,0 +1,14 @@
+begin
+  u = data_bag_item('users', Etc.getlogin)
+rescue Net::HTTPServerException => e
+  Chef::Application.fatal!('#{cookbook_name} could not load data bag; #{e}')
+end
+
+node.default["rbenv"]["group_users"] = ["#{u['id']}"]
+
+include_recipe "rbenv"
+include_recipe "rbenv::ruby_build"
+
+#rbenv_ruby "2.0.0.p0" do
+#  #global true
+#end
