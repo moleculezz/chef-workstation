689f48328866ea01ce5456a881f52007e4f76e92
G. Arends <gdarends@gmail.com>
2013-03-24 10:49:08 +0100

    disable default nginx site

--- cookbooks/workstation/recipes/nginx.rb
+++ cookbooks/workstation/recipes/nginx.rb
@@ -4,6 +4,7 @@ rescue Net::HTTPServerException => e
   Chef::Application.fatal!("#{cookbook_name} could not load data bag; #{e}")
 end
 
+node.default["nginx"]["default_site_enabled"] = false
 include_recipe "nginx"
 
 directory "/var/www/nginx-default" do
